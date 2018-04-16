d0d58a3fb1540abfff27f7c22fa0af740f0b59e4
Murberrytree <k23sh138@gmail.com>
2018-04-17 05:26:11 +0900

    'vueadmin'

---
+++ src/views/user/components/FilterBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="filter-bar control is-grouped">
+    <p class="control is-expanded has-addons">
+      <input id="search" class="input"
+       type="text" v-model="filterText"
+       @keyup.enter="doFilter"
+       placeholder="이름 또는 이메일을 입력하세요">
+      <a @click="doFilter" class="button is-primari">검색</a>
+      <a @click="resetFilter" class="button">리셋</a>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'FilterBar',
+    data () {
+      return {
+        filterText:''
+      }
+    },
+    methods:{
+      doFilter () {
+        this.$events.fire('filter-set', this.filterText)
+      },
+      resetFilter () {
+        this.filterText=''
+        this.$events.fire('filter-reset')
+      }
+    }
+  }
+</script>
+
+<style>
+  #search {
+    width:300px;
+  }
+</style>
